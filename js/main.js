5e17edb9c793605e7396256d79f1ce0b485dd852
cbstodd <cbstodd@gmail.com>
2018-01-18 00:11:24 -0500

    Added videos and powerpoint for technocap

--- js/main.js
+++ js/main.js
@@ -54,6 +54,7 @@ $(document).ready(function() {
 
     //parallax effect initialization
     $('.hero').parallax('50%', 0.3);
+    $('.parallax-container').parallax('50%', 0.3);
 
 
     //Nice scroll initialization
@@ -137,4 +138,33 @@ $(document).ready(function() {
     });
 
 
+
+});
+
+
+
+// Banner video;
+$(document).ready(function () {
+    $(window).on('load scroll', function () {
+        var scrolled = $(this).scrollTop();
+        $('#title').css({
+            'transform': 'translate3d(0, ' + -(scrolled * 0.2) + 'px, 0)', // parallax (20% scroll rate)
+            'opacity': 1 - scrolled / 400 // fade out at 400px from top
+        });
+        $('#hero-vid').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
+    });
+
+    // video controls
+    $('#state').on('click', function () {
+        var video = $('#hero-vid').get(0);
+        var icons = $('#state > span');
+        $('#overlay').toggleClass('fade');
+        if (video.paused) {
+            video.play();
+            icons.removeClass('fa-play').addClass('fa-pause');
+        } else {
+            video.pause();
+            icons.removeClass('fa-pause').addClass('fa-play');
+        }
+    });
 });

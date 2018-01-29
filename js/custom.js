a69343597d635d71046c2e57049523423f593796
cbstodd <cbstodd@gmail.com>
2018-01-29 12:02:00 -0500

    Cleared README.md bc it was showing in search results

--- js/custom.js
+++ js/custom.js
@@ -2,50 +2,57 @@
  * Created by cstodd on 4/14/16.
  */
 
-$(document).ready(function (){
+$(document).ready(function() {
     // function showZoomMsg(){
     //     $('h4.zoomOutMsg').show('1100');
     // }
     $('span.zoomOutMsg').hide();
     $('small.zoomMsg, .zoomMsg2, .zoomMsg3, .zoomMsg4').css('opacity', '0');
 
-    $('p > img.zoomTarget').click(function (){
+    $('p > img.zoomTarget').click(function() {
         $('span.zoomOutMsg').show('1100');
 
-    })
+    });
 
-    $('.fadeInLeft').click(function (){
+    $('.fadeInLeft').click(function() {
         $('span.zoomOutMsg').hide('fast');
 
-    })
+    });
 
-    $('.iphone > p > img.pic1').mouseover(function (){
+    $('.iphone > p > img.pic1').mouseover(function() {
         $('small.zoomMsg').css('opacity', '1.0');
-    })
-    $('.iphone > p > img.pic1').mouseleave(function (){
+    });
+    $('.iphone > p > img.pic1').mouseleave(function() {
         $('small.zoomMsg').css('opacity', '0');
-    })
+    });
 
-    $('.iphone > p > img.pic2').mouseover(function (){
+    $('.iphone > p > img.pic2').mouseover(function() {
         $('small.zoomMsg2').css('opacity', '1.0');
-    })
-    $('.iphone > p > img.pic2').mouseleave(function (){
+    });
+    $('.iphone > p > img.pic2').mouseleave(function() {
         $('small.zoomMsg2').css('opacity', '0');
-    })
+    });
 
-    $('.iphone > p > img.pic3').mouseover(function (){
+    $('.iphone > p > img.pic3').mouseover(function() {
         $('small.zoomMsg3').css('opacity', '1.0');
-    })
-    $('.iphone > p > img.pic3').mouseleave(function (){
+    });
+    $('.iphone > p > img.pic3').mouseleave(function() {
         $('small.zoomMsg3').css('opacity', '0');
-    })
+    });
 
-    $('.iphone > p > img.pic4').mouseover(function (){
+    $('.iphone > p > img.pic4').mouseover(function() {
         $('small.zoomMsg4').css('opacity', '1.0');
-    })
-    $('.iphone > p > img.pic4').mouseleave(function (){
+    });
+    $('.iphone > p > img.pic4').mouseleave(function() {
         $('small.zoomMsg4').css('opacity', '0');
-    })
+    });
 
+
+    // Jarallax
+
+    $('.jarallax').jarallax({
+        speed: 0.2
+    });
 
 });
+
